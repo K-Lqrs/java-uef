a496b160941b1c4c1c28b913282d174cc8dfe4a0
Ruxy <tomatotouroku0001@gmail.com>
2024-09-08 03:28:03 +1200

    update native method

--- native/UefApp.cpp
+++ native/UefApp.cpp
@@ -1,11 +1,9 @@
 #include "UefApp.h"
 
-#include <AppCore/Window.h>
 #include <AppCore/App.h>
 
 extern "C" {
 using namespace ultralight;
-RefPtr<Window> g_windowInstance;  // グローバル変数でウィンドウインスタンスを保持
 
 JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_createApp(JNIEnv *env, jobject obj, jobject configObj, jobject settingsObj) {
     jclass settingsClass = env->GetObjectClass(settingsObj);
@@ -44,51 +42,8 @@ JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_createApp(JNIEnv *env, jobject 
     App::Create(settings);
 }
 
-JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_createWindow(JNIEnv *env, jobject obj, jint width, jint height, jboolean fullscreen, jint flags) {
-    if (App::instance() != nullptr) {
-        auto monitor = App::instance()->main_monitor();
-
-        bool isFullscreen = static_cast<bool>(fullscreen);
-
-        int windowFlags = 0;
-        if (flags & kWindowFlags_Borderless) {
-            windowFlags |= kWindowFlags_Borderless;
-        }
-        if (flags & kWindowFlags_Titled) {
-            windowFlags |= kWindowFlags_Titled;
-        }
-        if (flags & kWindowFlags_Resizable) {
-            windowFlags |= kWindowFlags_Resizable;
-        }
-        if (flags & kWindowFlags_Maximizable) {
-            windowFlags |= kWindowFlags_Maximizable;
-        }
-        if (flags & kWindowFlags_Hidden) {
-            windowFlags |= kWindowFlags_Hidden;
-        }
-
-        g_windowInstance = Window::Create(monitor, width, height, isFullscreen, windowFlags);
-    }
-}
-
-JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_setWindowTitle(JNIEnv *env, jobject obj, jstring title) {
-    if (g_windowInstance) {
-        const char *nativeTitle = env->GetStringUTFChars(title, nullptr);
-        g_windowInstance->SetTitle(nativeTitle);
-        env->ReleaseStringUTFChars(title, nativeTitle);
-    }
-}
-
-JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_closeWindow(JNIEnv *env, jobject obj) {
-    if (g_windowInstance) {
-        g_windowInstance->Close();
-        g_windowInstance = nullptr;
-    }
-}
-
 JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_run(JNIEnv *env, jobject obj) {
     if (App::instance() != nullptr) {
-        g_windowInstance->Show();
         App::instance()->Run();
     }
 }
--- native/UefApp.h
+++ native/UefApp.h
@@ -7,13 +7,6 @@ extern "C" {
 JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_createApp(JNIEnv *env, jobject obj, jobject configObj,
                                                            jobject settingsObj);
 
-JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_createWindow(JNIEnv *env, jobject obj, jint width, jint height,
-                                                              jboolean fullscreen, jint flags);
-
-JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_setWindowTitle(JNIEnv *env, jobject obj, jstring title);
-
-JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_closeWindow(JNIEnv *env, jobject obj);
-
 JNIEXPORT void JNICALL Java_net_rk4z_juef_UefApp_run(JNIEnv *env, jobject obj);
 }
 
